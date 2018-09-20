1c6bdbda0c00907079f1e05dd8099986548fd430
thomaslule <thomas@lule.fr>
2018-09-20 16:35:24 +0200

    publishAndApply returns the created event

--- src/entity.ts
+++ src/entity.ts
@@ -20,11 +20,12 @@ export abstract class Entity<TDecision> {
     return this.decisionProjection.getState().decision;
   }
 
-  protected async publishAndApply(eventData: { [x: string]: any }) {
+  protected async publishAndApply(eventData: { [x: string]: any }): Promise<Event> {
     const sequence = this.decisionProjection.getState().sequence + 1;
     const insertDate = new Date().toISOString();
     const event: Event = { ...eventData, aggregate: this.getAggregate(), id: this.id, sequence, insertDate };
     this.decisionProjection.handleEvent(event);
     await this.publish(event, this.decisionProjection.getState());
+    return event;
   }
 }
